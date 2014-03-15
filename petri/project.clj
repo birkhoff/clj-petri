ae1a737075d9e45ebf40965f4e9823d8faca9be9
birkhoff <papstVII@gmx.de>
2014-03-15 19:02:34 +0100

    updated project

--- petri/project.clj
+++ petri/project.clj
@@ -1,6 +1,6 @@
 (defproject petri "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Petri Netz Abschluss Projekt"
+  :url "https://github.com/birkhoff/clj-petri"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
